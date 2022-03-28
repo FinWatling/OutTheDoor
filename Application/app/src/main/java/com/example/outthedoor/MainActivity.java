173e6d9d76d9784c2cfde40f041a2f4c243eb5b7
finlaywatling <finlaywatling@gmail.com>
2022-03-28 20:41:47 +0100

    added alarm functionality

--- Application/app/src/main/java/com/example/outthedoor/MainActivity.java
+++ Application/app/src/main/java/com/example/outthedoor/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.outthedoor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -15,12 +19,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView time = (TextView) findViewById(R.id.time);
+        TextView time = findViewById(R.id.time);
 
         Date currentTime = new Date();
         String currentDateTimeString = DateFormat.getTimeInstance(DateFormat.SHORT).format(currentTime);
 
         time.setText(currentDateTimeString);
+
+        Button sleepingNowButton = findViewById(R.id.sleepingNowButton);
+        sleepingNowButton.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+
+                Intent setAlarm = new Intent(AlarmClock.ACTION_SET_ALARM);
+                setAlarm.putExtra(AlarmClock.EXTRA_HOUR, 8);
+                setAlarm.putExtra(AlarmClock.EXTRA_MINUTES, 30);
+                startActivity(setAlarm);
+
+            }
+
+
+        });
 
     }
 }
